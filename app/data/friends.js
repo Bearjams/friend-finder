aad0326fcd6529b357b3d4eb48ef2631d00a6e79
bearjam <sam.croce@gmail.com>
2020-07-11 12:41:59 -0400

    working

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendList = [
+    {
+      name: 'Sam Croce',
+      profilePic: 'https://avatars1.githubusercontent.com/u/61466334?s=400&u=31225843fc6e7e86a9d2489469362ec22f327cdf&v=4',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  module.exports = friendList;
